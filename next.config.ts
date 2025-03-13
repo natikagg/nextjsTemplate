9faa3dfbbfe0d4e7a2981073d40311221820fbd9
natikagg <143027823+natikagg@users.noreply.github.com>
2025-03-12 19:38:45 -0700

    changs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 // Replace 'your-repo-name' with your actual GitHub repository name
 const repoName = 'nextjsTemplate'; // Example: 'my-nextjs-app'
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV == 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
